d00d68ce41315ad577c1941b465f1bfb1db64af1
cap <christian@apsel-mv.de>
2020-10-17 09:53:01 +0200

    Create 2 Datastructures for the grid, create some Tests for each, update

---
+++ src/test/java/GridPointsTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import java.awt.*;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class GridPointsTest {
+    @Test
+    public void testCreateGrid(){
+        HashMap<Point, Boolean> testGrid = new HashMap<>();
+        testGrid.put(new Point(0, 0), false);
+        assertEquals(testGrid, new GridPoints(1,1).getGrid());
+    }
+    @Test
+    public void testChangeValue(){
+        HashMap<Point, Boolean> testGrid = new HashMap<>();
+        testGrid.put(new Point(0, 0), true);
+        GridPoints currentGrid = new GridPoints(1,1);
+        currentGrid.changeValue(new Point(0,0));
+        assertEquals(testGrid, currentGrid.getGrid());
+    }
+
+}
