9ef8e89f51153c63f1837cf3b39897012b5becd4
Fabian Forthmann <fabian.forthmann@ppi.de>
2020-10-17 18:01:32 +0200

    Added config reader and possible config
    The config still has to be filled with usefull content

--- src/main/java/ConfigReader.java
+++ src/main/java/ConfigReader.java
@@ -1,2 +1,38 @@
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ConfigReader {
+
+    private final Properties prop = new Properties();
+    private static final String PROPERTIES_FILE_NAME = "config.properties";
+
+    public ConfigReader() {
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME);
+            prop.load(inputStream);
+        } catch (Exception e) {
+            // LOGGER.error("The properties file with the name '{}' could not be found. Closing the program without sending a file", PROPERTIES_FILE_NAME);
+            throw new RuntimeException("The properties file could not be found or loaded.");
+        }
+    }
+
+    public String getGameType() {
+        return prop.getProperty("game.type");
+    }
+
+    public String getModel() {
+        return prop.getProperty("game.model");
+    }
+
+    public String getDatastructure() {
+        return prop.getProperty("game.datastructure");
+    }
+
+    public String getLoggingType() {
+        return prop.getProperty("game.loggingType");
+    }
+
+    public int getTimeToLive() {
+        return Integer.parseInt(prop.getProperty("game.timeToLive"));
+    }
 }
