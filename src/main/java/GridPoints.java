15b15e2172dfd44bf6cc9752fcfe0c797fb6b9d1
Christian Apsel - A18  - 16778 <christian.apsel@nordakademie.de>
2020-10-17 20:25:04 +0200

    Simplify getMooreNeighbors, rearrange code

--- src/main/java/GridPoints.java
+++ src/main/java/GridPoints.java
@@ -12,28 +12,16 @@ public class GridPoints extends Grid implements IGrid {
         createGrid();
     }
 
-    private void createGrid() {
-        grid = new HashMap<>();
-        for (int i = 0; i < depth; i++) {
-            for (int j = 0; j < length; j++) {
-                grid.put(new Point(j, i), false);
-            }
-        }
-    }
-    @Override
-    boolean checkFieldExists(int x, int y) {
-        return x >= 0 && x <= depth - 1 && y >= 0 && y <= length - 1;
-    }
-
     @Override
     public void changeValue(Point position) {
-        if (getGrid().get(position).equals(false)) {
-            getGrid().replace(position, true);
-        } else {
-            getGrid().replace(position, false);
-        }
+        getGrid().replace(position, getGrid().get(position).equals(false));
 
 
+    }
+
+    @Override
+    public HashMap<Point, Boolean> getGrid() {
+        return grid;
     }
 
     @Override
@@ -42,12 +30,18 @@ public class GridPoints extends Grid implements IGrid {
     }
 
     @Override
-    public HashMap<Point, Boolean> getGrid() {
-        return grid;
+    boolean checkFieldExists(int x, int y) {
+        return x >= 0 && x <= depth - 1 && y >= 0 && y <= length - 1;
     }
 
-
-
+    private void createGrid() {
+        grid = new HashMap<>();
+        for (int i = 0; i < depth; i++) {
+            for (int j = 0; j < length; j++) {
+                grid.put(new Point(j, i), false);
+            }
+        }
+    }
 
 
 }
