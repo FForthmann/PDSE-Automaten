9aeae3c9517e32fe1378c3c825095322422c0f3d
Georg Mezlaw - A18 - 16595 <georg.mezlaw@nordakademie.de>
2020-10-17 15:50:39 +0200

    add a test for getVonNeumannNeighbors and getMooreNeighbors for GridArrayTest

--- src/test/java/GridArrayTest.java
+++ src/test/java/GridArrayTest.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GridArrayTest {
 
@@ -21,4 +23,33 @@ public class GridArrayTest {
         assertEquals(testGrid, currentGrid.getGrid());
 
     }
+
+
+    @Test
+    public void testGetVonNeumannNeighbors() {
+        ArrayList<Point> expectedValues = new ArrayList<>();
+        expectedValues.add(new Point(0, 0));
+        expectedValues.add(new Point(1, 1));
+        expectedValues.add(new Point(2, 0));
+        GridPoints currentGrid = new GridPoints(4, 4);
+
+        assertTrue(expectedValues.containsAll(currentGrid.getVonNeumannNeighbors(new Point(1, 0)))
+                && currentGrid.getVonNeumannNeighbors(new Point(1, 0)).size() == expectedValues.size());
+
+    }
+
+    @Test
+    public void testGetMooreNeighbors() {
+        ArrayList<Point> expectedValues = new ArrayList<>();
+        expectedValues.add(new Point(0, 0));
+        expectedValues.add(new Point(0, 1));
+        expectedValues.add(new Point(1, 1));
+        expectedValues.add(new Point(2, 1));
+        expectedValues.add(new Point(2, 0));
+        GridPoints currentGrid = new GridPoints(4, 4);
+
+        assertTrue(expectedValues.containsAll(currentGrid.getMooreNeighbors(new Point(1, 0)))
+                && currentGrid.getMooreNeighbors(new Point(1, 0)).size() == expectedValues.size());
+    }
+
 }
