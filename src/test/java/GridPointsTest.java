da6a1504606135b969c40c9c42c03b95399613af
Georg Mezlaw - A18 - 16595 <georg.mezlaw@nordakademie.de>
2020-10-17 13:13:34 +0200

    add a test for getVonNeumannNeighbors

--- src/test/java/GridPointsTest.java
+++ src/test/java/GridPointsTest.java
@@ -1,26 +1,40 @@
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GridPointsTest {
     @Test
-    public void testCreateGrid(){
+    public void testCreateGrid() {
         HashMap<Point, Boolean> testGrid = new HashMap<>();
         testGrid.put(new Point(0, 0), false);
-        assertEquals(testGrid, new GridPoints(1,1).getGrid());
+        assertEquals(testGrid, new GridPoints(1, 1).getGrid());
     }
+
     @Test
-    public void testChangeValue(){
+    public void testgetVonNeumannNeighbors() {
         HashMap<Point, Boolean> testGrid = new HashMap<>();
         testGrid.put(new Point(0, 0), true);
-        GridPoints currentGrid = new GridPoints(1,1);
-        currentGrid.changeValue(new Point(0,0));
+        GridPoints currentGrid = new GridPoints(1, 1);
+        currentGrid.changeValue(new Point(0, 0));
         assertEquals(testGrid, currentGrid.getGrid());
+    }
 
+    public void testNeighbors() {
+        ArrayList<Point> expectedValues = new ArrayList<>();
+        expectedValues.add(new Point(0, 0));
+        expectedValues.add(new Point(1, 2));
+        expectedValues.add(new Point(2, 0));
+        GridPoints currentGrid = new GridPoints(4, 4);
+
+        assertTrue(expectedValues.containsAll(currentGrid.getVonNeumannNeighbors(new Point(1, 0)))
+                && currentGrid.getVonNeumannNeighbors(new Point(1, 0)).size() == expectedValues.size());
 
     }
+
 
 }
