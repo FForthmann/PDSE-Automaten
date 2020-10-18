74aa6302571cef82868cb827f9174f6aa276cc16
Christian Apsel - A18  - 16778 <christian.apsel@nordakademie.de>
2020-10-18 14:55:40 +0200

    Add test method for GridFactory

--- src/test/java/GridFactoryTest.java
+++ src/test/java/GridFactoryTest.java
@@ -1,9 +1,17 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class GridFactoryTest {
 
     @Test
     public void getGrid() {
-
+        ConfigReader configReader = mock(ConfigReader.class);
+        when(configReader.getDatastructure()).thenReturn("0");
+        GridFactory gridFactory = new GridFactory(4, 4);
+        IGrid currentGrid = new GridPoints(4, 4);
+        assertEquals(gridFactory.getGrid(), currentGrid);
     }
 }
