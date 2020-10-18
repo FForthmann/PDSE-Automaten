3a3a7d5121c391028a026fa1f6aaf9bbbbbf59c5
Christian Apsel - A18  - 16778 <christian.apsel@nordakademie.de>
2020-10-18 13:31:22 +0200

    give interface 2 Methods getMooreNeighbors, getVonNeumannNeighbors to access at objecttype IGrid

--- src/main/java/IGrid.java
+++ src/main/java/IGrid.java
@@ -1,9 +1,14 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public interface IGrid {
     void changeValue(Point position);
 
     Object getGrid();
+
+    ArrayList<Point> getMooreNeighbors(Point point);
+
+    ArrayList<Point> getVonNeumannNeighbors(Point point);
 
     boolean getValue(Point position);
 
