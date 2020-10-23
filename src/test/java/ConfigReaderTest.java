7692e7c2db7c08373b8b09243df79ce3dd1a07e1
Fabian Forthmann <fabian.forthmann@ppi.de>
2020-10-23 17:52:16 +0200

    Created Test for ConfigReader with variable filename and implemented it

--- src/test/java/ConfigReaderTest.java
+++ src/test/java/ConfigReaderTest.java
@@ -1,43 +1,58 @@
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mock;
 
+import java.io.*;
+import java.lang.annotation.Repeatable;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
 public class ConfigReaderTest {
-    @Mock
-    Properties properties = new Properties();
+
+    private final static String GAME_TYPE = "1";
+    private final static String MODEL = "2";
+    private static final String DATASTRUCTURE = "3";
+    private final static String LOGGING_TYPE = "4";
+    private final static String TIME_TO_LIVE = "5";
+    private final static String GRID_LENGTH = "6";
+    private final static String GRID_WIDTH = "7";
+    private final static String TERMINATION_TYPE = "8";
+
+    @BeforeClass
+    public static void doBefore() throws IOException, InterruptedException {
+        Properties properties = new Properties();
+
+        properties.setProperty("game.type", GAME_TYPE);
+        properties.setProperty("game.model", MODEL);
+        properties.setProperty("game.datastructure", DATASTRUCTURE);
+        properties.setProperty("game.loggingType", LOGGING_TYPE);
+        properties.setProperty("game.timeToLive", TIME_TO_LIVE);
+        properties.setProperty("game.grid.length", GRID_LENGTH);
+        properties.setProperty("game.grid.width", GRID_WIDTH);
+        properties.setProperty("game.terminationType", TERMINATION_TYPE);
+
+        // Changing String to ByteArray and getting it into the right format
+        byte[] bytes = properties.toString().replaceAll(",", "\n").replaceFirst("\\{", "").replaceAll("}", "").replaceAll(" ", "").getBytes();
+        try (OutputStream outputStream = new FileOutputStream("src\\main\\resources\\configTest.properties")) {
+            outputStream.write(bytes);
+        }
+
+        TimeUnit.SECONDS.sleep(10);
+    }
 
     @Test
     public void readPropertiesWithValues() {
-        ConfigReader configReader = new ConfigReader();
+        ConfigReader configReader = new ConfigReader("configTest.properties");
 
-        String gameType = "1";
-        String model = "2";
-        String datastructure = "3";
-        String loggingType = "4";
-        String timeToLive = "5";
-        String gridLength = "6";
-        String gridWidth = "7";
-        String terminationType = "8";
-
-        properties.setProperty("game.type", gameType);
-        properties.setProperty("game.model", model);
-        properties.setProperty("game.datastructure", datastructure);
-        properties.setProperty("game.loggingType", loggingType);
-        properties.setProperty("game.timeToLive", timeToLive);
-        properties.setProperty("game.grid.length", gridLength);
-        properties.setProperty("game.grid.width", gridWidth);
-        properties.setProperty("game.terminationType", terminationType);
-
-        assertEquals(gameType, configReader.getGameType());
-        assertEquals(model, configReader.getModel());
-        assertEquals(datastructure, configReader.getDatastructure());
-        assertEquals(loggingType, configReader.getLoggingType());
-        assertEquals(Integer.parseInt(timeToLive), configReader.getTimeToLive());
-        assertEquals(Integer.parseInt(gridLength), configReader.getGridLength());
-        assertEquals(Integer.parseInt(gridWidth), configReader.getGridWidth());
-        assertEquals(terminationType, configReader.getTerminationType());
+        assertEquals(GAME_TYPE, configReader.getGameType());
+        assertEquals(MODEL, configReader.getModel());
+        assertEquals(DATASTRUCTURE, configReader.getDatastructure());
+        assertEquals(LOGGING_TYPE, configReader.getLoggingType());
+        assertEquals(Integer.parseInt(TIME_TO_LIVE), configReader.getTimeToLive());
+        assertEquals(Integer.parseInt(GRID_LENGTH), configReader.getGridLength());
+        assertEquals(Integer.parseInt(GRID_WIDTH), configReader.getGridWidth());
+        assertEquals(TERMINATION_TYPE, configReader.getTerminationType());
     }
 }
