5562769b6a25f0b09077aeab8e93c103f61b9b95
username <username@domaim.com>
2020-10-25 14:33:21 +0100

    create test for CopyGrid.java

---
+++ src/test/java/CopyGridTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class CopyGridTest {
+    @Test
+    public void testGetGrid() {
+        IGrid originalGrid = new GridPoints(5, 5);
+        CopyGrid copyGrid = new CopyGrid(originalGrid);
+        originalGrid.setValue(new Point(0, 0), true);
+        assertNotEquals(originalGrid.getValue(new Point(0, 0)), copyGrid.getGrid().getValue(new Point(0, 0)));
+    }
+}
