1b19fa74bdbb49b6db3f074dc32a416078ea450e
Fabian Forthmann <fabian.forthmann@ppi.de>
2020-10-27 20:16:41 +0100

    Added valid testData for ConfigReader tests and fixed boolean logic in validator

--- src/main/java/de/nordakademie/pdse/config/PropertyValidator.java
+++ src/main/java/de/nordakademie/pdse/config/PropertyValidator.java
@@ -4,43 +4,43 @@ import java.util.Properties;
 
 public class PropertyValidator {
 
-    private static final String GAME_TYPE = "game.type";
-    private static final String GAME_MODEL = "game.model";
-    private static final String DATA_STRUCTURE = "game.dataStructure";
-    private static final String LOGGING_TYPE = "game.loggingType";
-    private static final String TIME_TO_LIVE = "game.timeToLive";
-    private static final String LENGTH = "game.grid.length";
-    private static final String WIDTH = "game.grid.width";
-    private static final String TERMINATION_TYPE = "game.terminationType";
+    private static final String GAME_TYPE_KEY = "game.type";
+    private static final String GAME_MODEL_KEY = "game.model";
+    private static final String DATA_STRUCTURE_KEY = "game.dataStructure";
+    private static final String LOGGING_TYPE_KEY = "game.loggingType";
+    private static final String TIME_TO_LIVE_KEY = "game.timeToLive";
+    private static final String LENGTH_KEY = "game.grid.length";
+    private static final String WIDTH_KEY = "game.grid.width";
+    private static final String TERMINATION_TYPE_KEY = "game.terminationType";
 
     public void validateConfig(Properties prop) {
         boolean validDataInFile = true;
         try {
-            if (("noChange".equals(prop.getProperty(TERMINATION_TYPE))) || ("ttlOrNoChange".equals(prop.getProperty(TERMINATION_TYPE))) || ("ttl".equals(prop.getProperty(TERMINATION_TYPE)))) {
-                if ("ttl".equals(prop.getProperty(TERMINATION_TYPE))) { // Hinzufügen ttlOrNoChange
-                    if (Integer.parseInt(prop.getProperty(TIME_TO_LIVE)) < 0) {
+            if (("noChange".equals(prop.getProperty(TERMINATION_TYPE_KEY))) || ("ttlOrNoChange".equals(prop.getProperty(TERMINATION_TYPE_KEY))) || ("ttl".equals(prop.getProperty(TERMINATION_TYPE_KEY)))) {
+                if ("ttl".equals(prop.getProperty(TERMINATION_TYPE_KEY))) { // Hinzufügen ttlOrNoChange
+                    if (Integer.parseInt(prop.getProperty(TIME_TO_LIVE_KEY)) < 0) {
                         validDataInFile = false;
                     }
                 }
             } else {
                 validDataInFile = false;
             }
-            if (!"Parity".equals(prop.getProperty(GAME_TYPE)) && !"GameOfLife".equals(prop.getProperty(GAME_TYPE))) {
+            if (!"Parity".equals(prop.getProperty(GAME_TYPE_KEY)) && !"GameOfLife".equals(prop.getProperty(GAME_TYPE_KEY))) {
                 validDataInFile = false;
             }
-            if (!"Moore".equals(prop.getProperty(GAME_MODEL)) && !"vonNeumann".equals(prop.getProperty(GAME_MODEL))) {
+            if (!"Moore".equals(prop.getProperty(GAME_MODEL_KEY)) && !"vonNeumann".equals(prop.getProperty(GAME_MODEL_KEY))) {
                 validDataInFile = false;
             }
-            if (!"GridPoints".equals(prop.getProperty(DATA_STRUCTURE)) && !"GridArray".equals(prop.getProperty(DATA_STRUCTURE))) {
+            if (!"GridPoints".equals(prop.getProperty(DATA_STRUCTURE_KEY)) && !"GridArray".equals(prop.getProperty(DATA_STRUCTURE_KEY))) {
                 validDataInFile = false;
             }
-            if (!"console".equals(prop.getProperty(LOGGING_TYPE)) && !"file".equals(prop.getProperty(LOGGING_TYPE)) && !"consoleAndFile".equals(prop.getProperty(LOGGING_TYPE)) && !"disable".equals(prop.getProperty(LOGGING_TYPE))) {
+            if (!"console".equals(prop.getProperty(LOGGING_TYPE_KEY)) && !"file".equals(prop.getProperty(LOGGING_TYPE_KEY)) && !"consoleAndFile".equals(prop.getProperty(LOGGING_TYPE_KEY)) && !"disable".equals(prop.getProperty(LOGGING_TYPE_KEY))) {
                 validDataInFile = false;
             }
-            if (Integer.parseInt(prop.getProperty(LENGTH)) < 1) {
+            if (Integer.parseInt(prop.getProperty(LENGTH_KEY)) < 1) {
                 validDataInFile = false;
             }
-            if (Integer.parseInt(prop.getProperty(WIDTH)) < 1) {
+            if (Integer.parseInt(prop.getProperty(WIDTH_KEY)) < 1) {
                 validDataInFile = false;
             }
         } catch (RuntimeException e) {
