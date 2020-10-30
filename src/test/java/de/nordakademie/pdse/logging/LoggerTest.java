f7217331c2dc45e3e7ef899c18177c009c71a59f
Christian Apsel - A18  - 16778 <christian.apsel@nordakademie.de>
2020-10-30 22:23:56 +0100

    add testMethode for Test Log and Console

--- src/test/java/de/nordakademie/pdse/logging/LoggerTest.java
+++ src/test/java/de/nordakademie/pdse/logging/LoggerTest.java
@@ -14,14 +14,14 @@ public class LoggerTest {
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-    @After
-    public void after() {
-        outputStreamCaptor.reset();
-    }
-
     @Before
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @After
+    public void after() {
+        outputStreamCaptor.reset();
     }
 
     @Test
@@ -31,14 +31,13 @@ public class LoggerTest {
                 "0000\n" +
                 "0000\n" +
                 "0000\n" + "\n\n";
-        System.setOut(new PrintStream(outputStreamCaptor));
         Logger log = new Logger("file");
         log.addGridToLog(new GridFactory(4, 4, "GridArray").getGrid().toString(), 2);
-        StringBuilder current= new StringBuilder();
+        StringBuilder current = new StringBuilder();
         String line;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(log.getFile()));
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 current.append(line).append("\n");
             }
         } catch (IOException e) {
@@ -60,10 +59,11 @@ public class LoggerTest {
         System.setOut(new PrintStream(outputStreamCaptor));
         Logger log = new Logger("console");
         log.addGridToLog(new GridFactory(4, 4, "GridArray").getGrid().toString(), 2);
-        int x  = 0;
         String current = outputStreamCaptor.toString();
         assertArrayEquals(expectedOutput.toCharArray(), current.toCharArray());
 
     }
+
+
 
 }
