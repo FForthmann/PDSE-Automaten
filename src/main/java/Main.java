8c29b378a4c84be499798f617437276c08439f7e
Christian Apsel - A18  - 16778 <christian.apsel@nordakademie.de>
2020-10-30 21:33:07 +0100

    fixing error in Main and rearrange code in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -40,15 +40,14 @@ public class Main {
             if (temp.contains(",")) {
                 String[] s = temp.split(",");
                 if (s.length == 2 && Integer.parseInt(s[0]) < configReader.getGridLength() && Integer.parseInt(s[1]) < configReader.getGridWidth()) {
-                    {
-                        game.getGird().setValue(new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1])), true);
-                    }
+                    game.getGird().setValue(new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1])), true);
                 } else if (temp.equalsIgnoreCase("run")) {
                     run = "run";
                 } else {
                     System.out.println("falsche Eingabe");
                 }
-
+            } else {
+                System.out.println("falsche Eingabe");
             }
 
         }
