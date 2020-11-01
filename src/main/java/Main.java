1057c72ace6232e92c5ebb833e77e2e1196d49fc
Christian Apsel - A18  - 16778 <christian.apsel@nordakademie.de>
2020-11-01 01:42:21 +0100

    repair userInterface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -41,11 +41,11 @@ public class Main {
                 String[] s = temp.split(",");
                 if (s.length == 2 && Integer.parseInt(s[0]) < configReader.getGridLength() && Integer.parseInt(s[1]) < configReader.getGridWidth()) {
                     game.getGird().setValue(new Point(Integer.parseInt(s[0]), Integer.parseInt(s[1])), true);
-                } else if (temp.equalsIgnoreCase("run")) {
-                    run = "run";
                 } else {
                     System.out.println("falsche Eingabe");
                 }
+            } else if (temp.equalsIgnoreCase("run")) {
+                run = "run";
             } else {
                 System.out.println("falsche Eingabe");
             }
