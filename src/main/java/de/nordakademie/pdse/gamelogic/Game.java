7f7279174e629ca60bc78307e5736fc81434c363
Rane Alexander Petersen 16818 <rane_alexander.petersen@norakademie.de>
2020-11-01 00:12:19 +0000

    (16818) Added new Constructor to Game
    This Constructor is used for the Experimentclasses to redirect the output into a file with a spicific name instead of the usual naming scheme

--- src/main/java/de/nordakademie/pdse/gamelogic/Game.java
+++ src/main/java/de/nordakademie/pdse/gamelogic/Game.java
@@ -34,7 +34,20 @@ public class Game {
         } else {
             this.continueGame = true;
         }
+    }
 
+    public Game(ConfigReader configReader, String loggingFileNAme) {
+        this.configReader = configReader;
+        this.timeToLive = configReader.getTimeToLive();
+        this.grid = new GridFactory(configReader.getGridLength(), configReader.getGridWidth(), configReader.getDatastructure()).getGrid();
+        this.gameType = new GameTypeFactory(configReader).getGameType();
+        this.logger = new Logger(configReader.getLoggingType(), loggingFileNAme);
+        this.iteration = 0;
+        if (configReader.getTerminationType().equals("ttl")) {
+            this.continueGame = timeToLive > 0;
+        } else {
+            this.continueGame = true;
+        }
     }
 
     private Boolean getContinueGame() {
